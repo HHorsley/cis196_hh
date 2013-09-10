0e952320f24c184c55415524c07763a2c050bc73
Hunty Dumpty <hunter.horsley@gmail.com>
2013-09-10 03:27:54 -0400

    New App in rails 3.2.3

---
+++ hh_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HhApp::Application.config.secret_token = '5d0da7d68da67f3374a7ffce7c7401a524cb309aa090a85ce0200e5fb3619f1702d9d017e81ed021fb36b1857620710c24b234e0fb0410d7619021bf38833101'
