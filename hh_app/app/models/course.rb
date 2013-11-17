db792af7a56347d5b94045ee7ce67e119c40e8c9
Hunty Dumpty <hunter.horsley@gmail.com>
2013-11-16 23:06:18 -0500

    remove edit and destory

--- hh_app/app/models/course.rb
+++ hh_app/app/models/course.rb
@@ -8,6 +8,11 @@ class Course < ActiveRecord::Base
   	validates :topic, presence: true, uniqueness: true
   	validates :user_id, presence: true
 
-    scope :rank, -> { order(:topic).limit(10)}
+    scope :home_top, -> { order(:votes_for).limit(3)}
+    scope :top, -> { order(:votes_for).limit(15)}
+    scope :home_editor_picks, -> { order(:editor_pick).limit(3)}
+    scope :editor_picks, -> { order(:editor_pick).limit(10)}
+    scope :home_weird, -> { order(:weird).limit(3)}
+    scope :weird, -> { order(:weird).limit(10)}
 
 end
