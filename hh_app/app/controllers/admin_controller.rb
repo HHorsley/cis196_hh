1aa11a2d3087dfa5d52c4e023e5c9d1092f12385
Hunty Dumpty <hunter.horsley@gmail.com>
2013-11-17 22:46:29 -0500

    fixed admin and home

--- hh_app/app/controllers/admin_controller.rb
+++ hh_app/app/controllers/admin_controller.rb
@@ -3,7 +3,7 @@ class AdminController < ApplicationController
 	def cockpit
   	@courses = Course.all
 
-    if !current_user_email = "hunter.horsley@gmail.com"
+    if !current_user.email == "hunter.horsley@gmail.com"
       redirect_to root_path
     end
 
